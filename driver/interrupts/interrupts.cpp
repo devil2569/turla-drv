1e7263846f93571e6c3b713acc2b7d7678b1518a
devil2569 <ivanpominov29@gmail.com>
2026-01-09 16:34:50 -0800

    Refactor interrupt handling and fix segment selector

--- driver/interrupts/interrupts.cpp
+++ driver/interrupts/interrupts.cpp
@@ -5,7 +5,7 @@ auto windows::interrupts::create_interrupt_gate(void* assembly_handler, segment_
 	segment_descriptor_interrupt_gate_64 gate;
 
 	gate.interrupt_stack_table = windows_gate.interrupt_stack_table;
-	gate.segment_selector = __readcs();
+	gate.segment_selector = read_cs();
 	gate.must_be_zero_0 = 0;
 	gate.type = SEGMENT_DESCRIPTOR_TYPE_INTERRUPT_GATE;
 	gate.must_be_zero_1 = 0;
@@ -31,6 +31,7 @@ auto windows::interrupts::initialize() -> project_status
 
 	RtlZeroMemory(constructed_idt_table, sizeof(segment_descriptor_interrupt_gate_64) * 256);
 
+	// copy windows idt
 	segment_descriptor_register_64 idt = { 0 };
 	__sidt(&idt);
 
@@ -45,5 +46,38 @@ auto windows::interrupts::initialize() -> project_status
 	constructed_idt_ptr.base_address = (uint64_t)constructed_idt_table;
 	constructed_idt_ptr.limit = (sizeof(segment_descriptor_interrupt_gate_64) * 256) - 1;
 
+
 	return status_success;
+}
+
+extern "C" void nmi_handler(trap_frame* ctx)
+{
+	uint64_t star_msr = __readmsr(IA32_STAR);
+	KPCR * kcpr = (KPCR*)__readgsqword(0x18);
+	uint64_t curr_user_panic_rip = 0;
+	rflags curr_user_rflags = { 0 };
+
+	curr_user_rflags.flags = ctx->rflags;
+	curr_user_rflags.interrupt_enable_flag = 1;
+	curr_user_rflags.virtual_8086_mode_flag = 0;
+	curr_user_rflags.reserved1 = 0;
+	curr_user_rflags.reserved2 = 0;
+	curr_user_rflags.reserved3 = 0;
+	curr_user_rflags.reserved4 = 0;
+	curr_user_rflags.read_as_1 = 1;
+	curr_user_rflags.nested_task_flag = 0;
+	curr_user_rflags.io_privilege_level = 3;
+	curr_user_rflags.resume_flag = 0;
+	curr_user_rflags.alignment_check_flag = 0;
+
+	uint16_t sysret_cs = (uint16_t)(((star_msr >> 48) + 16) | 3); 
+	uint16_t sysret_ss = (uint16_t)(((star_msr >> 48) + 8) | 3);   
+
+	ctx->rsp = kcpr->UserRsp;
+	ctx->rip = curr_user_panic_rip;
+	ctx->rflags = curr_user_rflags.flags;
+	ctx->cs_selector = sysret_cs;
+	ctx->ss_selector = sysret_ss;
+
+	swap_gs();
 }
