c10de25c787c778a5c97201dc91f6f17c165924c
devil2569 <ivanpominov29@gmail.com>
2026-01-09 16:35:00 -0800

    Refactor interrupt definitions to use constexpr

--- driver/interrupts/interrupts.h
+++ driver/interrupts/interrupts.h
@@ -8,7 +8,7 @@ namespace windows {
         Definitions
     */
 
-    #define  SEGMENT_DESCRIPTOR_TYPE_INTERRUPT_GATE 0xE;
+    constexpr auto SEGMENT_DESCRIPTOR_TYPE_INTERRUPT_GATE = 0xE;
 
     /*
         Global variables
@@ -17,8 +17,6 @@ namespace windows {
     segment_descriptor_register_64 constructed_idt_ptr = { 0 };
 
     segment_descriptor_interrupt_gate_64* constructed_idt_table = 0;
-
-    uint64_t g_windows_nmi_handler;
 
     /*
         Utility & Initialization
@@ -30,9 +28,12 @@ namespace windows {
         auto initialize() -> project_status;
 
         auto create_interrupt_gate(void* assembly_handler, segment_descriptor_interrupt_gate_64 windows_gate) -> segment_descriptor_interrupt_gate_64;
+
     };
 }
 
 extern windows::interrupts turla_interrupts;
 
-#define IA32_STAR 0xC0000081
+constexpr auto IA32_STAR = 0xC0000081;
+
+extern "C" void nmi_handler(trap_frame* ctx);
